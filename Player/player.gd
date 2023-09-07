c050d292234a81e94a11cf8cdf02bf9c8141c086
xLTJ <lxcy.3@proton.me>
2023-09-08 01:00:10 +0200

    implemented the following: player movement, player animations (placeholder), tilemap and world (placeholder) and following camara with borders

--- Player/player.gd
+++ Player/player.gd
@@ -1,11 +1,36 @@
 extends CharacterBody2D
 
-@export var speed: int = 40
+@export var speed: int = 200
+@onready var player_animation = $AnimationPlayer
+@onready var player_sprite = $Sprite2D
+var direction = 'up' # Direction the player faces. Default is up
 
-func playerInput():
-	var movementDirection = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
-	velocity = movementDirection * speed
+# Gets the input of the player and sets the players velocity. get_vector specefies four actions for the positive and negative X and Y axes. For instance 'move_left' is for negative_x (aka left)
+func player_input():
+	var movement_direction = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
+	
+	if movement_direction.length() > 0:
+		velocity = movement_direction.normalized() * speed
+	else: velocity = Vector2.ZERO # without this the player keeps moving when the key is released
 
+# Animates the player depending on their move direction
+func player_animation_update():
+	var state = 'idle' # Default state is idle, if the vector is not equal to 0 (meaning the player moves) the state will be set to moving
+	if velocity.length() != 0: # if the player moves the direction of the player will be changed. This direction determines what way the player faces, and is kept until the player moves another way
+		state = 'moving'
+		if velocity.y > 0: direction = 'down'
+		elif velocity.y < 0: direction = 'up'
+		elif velocity.x != 0: direction = 'horizontal'
+		player_sprite.flip_h = velocity.x < 0 # flips the sprite so the player faces left if theyre moving left
+	
+	# Plays the moving or the idle animation depending on the state
+	if state == 'moving':
+		player_animation.play('walk_' + direction)
+	else: 
+		player_animation.play('idle_' + direction)
+
+# This runs every frame
 func _physics_process(delta):
-	playerInput()
+	player_input()
 	move_and_slide()
+	player_animation_update()
---
+++ World/follow cam.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@export var tilemap: TileMap
+
+func _ready():
+	var world_borders = tilemap.get_used_rect() # Gets the amount of tiles used
+	var tile_size = tilemap.cell_quadrant_size # Gets tje size of each tile in pixels
+	var world_size = world_borders.size * tile_size # The size of the world in pixels
+	
+	# Sets the limits of the camara to the size of the world
+	limit_right = world_size.x
+	limit_bottom = world_size.y
